ac23f96cc3142b0393507596cf6ef390a5d42e43
foreveryh <foreveryh@gmail.com>
2012-09-04 17:27:51 +0800

    import userena model

--- langkawi/forms.py
+++ langkawi/forms.py
@@ -1,7 +1,9 @@
 from django import forms
 from django.utils.translation import gettext as _
-
 from django.contrib.auth.models import User
+from userena import settings as userena_settings
+from userena.models import UserenaSignup
+from userena.utils import get_profile_model
 
 
 class UserForm(forms.Form):
