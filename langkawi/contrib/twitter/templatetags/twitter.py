26bf81f33856bbca6641f94129f7c46dcef57251
foreveryh <foreveryh@gmail.com>
2012-09-11 13:52:26 +0800

    remove twitter contrib

--- langkawi/contrib/twitter/templatetags/twitter.py
+++
@@ -1,6 +0,0 @@
-from django import template
-from langkawi.templatetags import button
-
-register = template.Library()
-
-register.tag('twitter_button', button('langkawi/twitter/twitter_button.html'))
--- langkawi/urls.py
+++ langkawi/urls.py
@@ -4,40 +4,15 @@ from views import Logout, Setup
 
 urlpatterns = patterns('',)
 
-if 'langkawi.contrib.openid' in settings.INSTALLED_APPS:
-    urlpatterns = urlpatterns + patterns('',
-        url(r'^openid/', include('langkawi.contrib.openid.urls',
-            namespace='openid')))
-
-if 'langkawi.contrib.twitter' in settings.INSTALLED_APPS:
-    urlpatterns = urlpatterns + patterns('',
-        url(r'^twitter/', include('langkawi.contrib.twitter.urls',
-            namespace='twitter')))
-
-if 'langkawi.contrib.linkedin' in settings.INSTALLED_APPS:
-    urlpatterns = urlpatterns + patterns('',
-        url(r'^linkedin/', include('langkawi.contrib.linkedin.urls',
-            namespace='linkedin')))
-
 if 'langkawi.contrib.facebook' in settings.INSTALLED_APPS:
     urlpatterns = urlpatterns + patterns('',
         url(r'^facebook/', include('langkawi.contrib.facebook.urls',
             namespace='facebook')))
 
-if 'langkawi.contrib.github' in settings.INSTALLED_APPS:
-    urlpatterns = urlpatterns + patterns('',
-        url(r'^github/', include('langkawi.contrib.github.urls',
-            namespace='github')))
-
 if 'langkawi.contrib.foursquare' in settings.INSTALLED_APPS:
     urlpatterns = urlpatterns + patterns('',
         url(r'^foursquare/', include('langkawi.contrib.foursquare.urls',
             namespace='foursquare')))
-
-if 'langkawi.contrib.tumblr' in settings.INSTALLED_APPS:
-    urlpatterns = urlpatterns + patterns('',
-        url(r'^tumblr/', include('langkawi.contrib.tumblr.urls',
-            namespace='tumblr')))
 
 if 'langkawi.contrib.instagram' in settings.INSTALLED_APPS:
     urlpatterns = urlpatterns + patterns('',
